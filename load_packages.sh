ed73876d4d1c71d1754203238a7e71fa545b4265
Micdu70 <Micdu70@users.noreply.github.com>
2020-07-10 12:39:47 +0200

    update load_packages.sh

--- load_packages.sh
+++ load_packages.sh
@@ -1,55 +1,76 @@
 #!/bin/bash
 
+printf "\n=========================================================\n\n"
+printf "            ~~~ RESTORE ANDROID PACKAGES ~~~\n\n"
+printf "=========================================================\n\n"
+
 # The following script will restore apps 
 # from a TWRP backup to an android phone.
-# Root adb access must be available.
+# Modified adbd service or Magisk must be used.
 
 # 1. Extract all the data volumes in the TWRP backup
-	# tar -xvf data.ext4.win000
-	# tar -xvf data.ext4.win001 etc.
+	# cat data.*.win??? | tar xvfi -
 # 2. Turn the bash script into an executable 
-	# chmod +x restore_android_packages.sh
+	# chmod +x load_packages.sh
 # 3. Run script
-	# ./restore_android_packages.
+	# ./load_packages.sh
 
 # The following resources were used in the creation of this script.
 # https://www.semipol.de/2016/07/30/android-restoring-apps-from-twrp-backup.html
 # https://itsfoss.com/fix-error-insufficient-permissions-device/
 
+#							#
+##						   ##
+#### BEGIN CONFIGURATION ####
+##						   ##
+#							#
+
 # TWRP extract location for data/data/
 localpackages='data/data/'
 # Android temp destination
-temppackages='/data/tmp/'
+temppackages='/sdcard/tmp/'
 # Android final destination
 remotepackages='/data/data/'
 
 # filename of packages in data/data/ to restore (one by line and between double quotes)
 declare -a packages=(
-"io.getdelta.android"
-"ca.mogo.mobile"
-"au.com.shiftyjelly.pocketcasts"
-"com.valvesoftware.android.steam.community"
-"com.ubercab"
-"de.stocard.stocard"
-"com.wealthsimple"
-"com.calm.android"
-"com.coinomi.wallet"
-"com.aspiro.tidal"
-"com.appgenix.bizcal"
-"com.americanexpress.android.acctsvcs.ca"
-"com.amazon.venezia"
+"balti.migrate"
 )
-# Get total number of packages to restore
-len=${#packages[@]}
 
-printf "=========================================================\n"
-printf "Starting ADB as root\n"
+#							#
+##						   ##
+####  END CONFIGURATION #####
+##						   ##
+#							#
+
+printf "=========================================================\n\n"
+printf "Executing 'adb root' command:\n\n"
 adb root
-printf "=========================================================\n"
+printf "\n"
+id=$(adb shell id -u)
+uid=$id
+if [[ $id != "0" ]] ; then
+	id=$(adb shell su -c "id -u")
+	if [ -z "$id" ] ; then
+		id=$uid
+	fi
+fi
+printf "\n[INFO] User ID of adb is '%s' (must be '0').\n\n" "$id"
+if [[ $id != "0" ]] ; then
+	printf "[ERROR] Didn't get root permissions! Can't restore.\n\n"
+	printf "[INFO] You can chose one of this two ways to be able to restore:\n\n- Install a modified adbd\n- Install Magisk and allow root for 'Shell' package\n\n"
+	printf "=========================================================\n\n"
+	exit
+else
+	printf "=========================================================\n\n"
+fi
 
-# Init counters
-# n = Number of actual package to restore
-# g = Number of app data successfully restored
+# get total number of packages to restore
+len=$(printf '%s\n' "${packages[@]}" | wc -w)
+
+# init counters
+# n = number of the actual package to be restored
+# g = number of app data successfully restored
 n=0
 g=0
 
@@ -58,25 +79,35 @@ do
 	n=$((n + 1))
 	printf "=========================================================\n\n"
 	printf "|***| Starting process for \"%s\" package (%s of %s)\n\n" "$package" "$n" "$len"
-	printf "[1/4] Checking device...\n"
-	userid=$(adb shell su -c "dumpsys package $package | grep userId | cut -d '=' -f2-")
+	if [ ! -d "$localpackages$package" ] ; then
+		printf "[WARN] Can't find this filename of package (%s).\n\n" "$localpackages$package"
+		continue
+	fi
+	printf "[***] Checking device...\n"
+	userid=$(adb shell dumpsys package "$package" | grep userId | cut -d '=' -f2-)
+	if [[ ! $userid =~ ^[0-9]+$ ]] ; then
+		userid=$(adb shell su -c "stat -c %u '$remotepackages$package'")
+	fi
 	if [[ $userid =~ ^[0-9]+$ ]] ; then
-		printf "[INFO] User ID is %s\n" "$userid"
-		printf "[2/4] Restoring data...\n"
+		printf "\n[INFO] User ID of this app is '%s'.\n\n" "$userid"
+		printf "[1/5] Pushing package to temp dir...\n"
 		adb push "$localpackages$package" "$temppackages$package"
-		adb shell su -c "cp -r $temppackages$package $remotepackages"
-		adb shell su -c "chown -R $userid:$userid $remotepackages$package"
-		adb shell su -c "restorecon -Rv $remotepackages$package"
+		printf "[2/5] Restoring package...\n"
+		adb shell su -c "cp -r '$temppackages$package' '$remotepackages'"
+		printf "[3/5] Correcting package...\n"
+		adb shell su -c "chown -R $userid:$userid '$remotepackages$package'"
+		adb shell su -c "restorecon -R '$remotepackages$package'"
+		printf "[4/5] Cleaning temp dir...\n"
+		adb shell su -c "rm -rf '$temppackages$package'"
+		printf "[5/5] Done!\n\n"
 		g=$((g + 1))
+		sleep 1
 	else
-		printf "[2/4] ERROR: App not found/installed! Can't restore...\n"
-	fi	
-	printf "[3/4] Cleaning...\n"
-	adb shell rm -rf "$temppackages$package"
-	printf "[4/4] Done!\n\n"
-	sleep 1
+		printf "\n[WARN] Can't read user ID info of this app. Can't restore.\n"
+		printf "[INFO] App must be installed on your device to be able to restore package.\n\n"
+	fi
 done
 
-printf "=========================================================\n"
-printf "      SUMMARY: %s / %s SUCCESSFULLY RESTORED\n" "$g" "$len"
-printf "=========================================================\n"
+printf "=========================================================\n\n"
+printf "      SUMMARY: %s / %s SUCCESSFULLY RESTORED\n\n" "$g" "$len"
+printf "=========================================================\n\n"
